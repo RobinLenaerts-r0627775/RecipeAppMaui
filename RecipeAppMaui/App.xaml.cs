5a766e8d1d8bcd61c536c4f908280e41ff1ff3d1
Robin Lenaerts <contact@robinlenaerts.dev>
2023-05-22 18:33:31 +0200

    Init

---
+++ RecipeAppMaui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace RecipeAppMaui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ RecipeAppMaui/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace RecipeAppMaui;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
